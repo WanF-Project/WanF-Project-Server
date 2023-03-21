b7e112e14f4ed7628c724a00532d928e131aa49a
kikingki <mongbu54@gmail.com>
2023-03-21 21:03:34 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/capstone/wanf/WanFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.wanf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanFApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
