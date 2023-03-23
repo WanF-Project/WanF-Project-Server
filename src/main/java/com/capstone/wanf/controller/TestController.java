e8481a42981f0c14769bd3e8602a1ddf56eacc19
qwe916 <qwe916@naver.com>
2023-03-23 15:15:01 +0900

    test: 배포 테스트

---
+++ src/main/java/com/capstone/wanf/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.capstone.wanf.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class TestController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("good");
+    }
+
+}
