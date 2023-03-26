ddea59e166e719ced9f7e1621cef320e6c3647ef
qwe916 <qwe916@naver.com>
2023-03-26 23:28:00 +0900

    feat: Major(전공), Professor(교수) entity 추가

---
+++ src/main/java/com/capstone/wanf/major/domain/entity/Major.java
@@ -0,0 +1,20 @@
+package com.capstone.wanf.major.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Table(name = "major")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Major {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
