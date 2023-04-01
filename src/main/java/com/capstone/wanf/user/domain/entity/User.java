f2ed79956b5cda52d423dcf244f1a251dc1aca9e
kikingki <mongbu54@gmail.com>
2023-04-01 23:07:34 +0900

    fix: User Entity, Repository 인증 번호 관련 수정

--- src/main/java/com/capstone/wanf/user/domain/entity/User.java
+++ src/main/java/com/capstone/wanf/user/domain/entity/User.java
@@ -1,15 +1,16 @@
 package com.capstone.wanf.user.domain.entity;
 
 import com.capstone.wanf.common.entity.BaseTimeEntity;
+import com.capstone.wanf.user.dto.request.UserRequest;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Entity
-@Table(name = "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Getter
+@Table(name = "user")
+@Entity
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +20,13 @@ public class User extends BaseTimeEntity {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "user_password", nullable = false)
+    @Column(name = "user_password")
     private String userPassword;
+
+    @Column(name = "verification_code", nullable = false)
+    private String verificationCode;
+
+    public void update(String userPassword) {
+        this.userPassword = userPassword;
+    }
 }
