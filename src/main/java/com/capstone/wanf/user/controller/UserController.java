e1c3392a6f3d84ad5ade85392d7e85fd1b7acdf5
kikingki <mongbu54@gmail.com>
2023-04-03 20:32:24 +0900

    fix: 유효성 검사 추가

--- src/main/java/com/capstone/wanf/user/controller/UserController.java
+++ src/main/java/com/capstone/wanf/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.capstone.wanf.user.dto.request.CodeRequest;
 import com.capstone.wanf.user.dto.request.EmailRequest;
 import com.capstone.wanf.user.dto.request.UserRequest;
 import com.capstone.wanf.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup/send-verification-code")
-    public ResponseEntity<Void> sendVerificationCode(@RequestBody EmailRequest emailRequest) {
+    public ResponseEntity<Void> sendVerificationCode(@Valid @RequestBody EmailRequest emailRequest) {
         // 인증번호 생성 및 전송
         String verificationCode = userService.generateVerificationCode();
         userService.sendVerificationCode(emailRequest, verificationCode);
@@ -27,13 +28,13 @@ public class UserController {
 
     @PostMapping("/signup/verify")
     public ResponseEntity<Void> verify(@RequestBody CodeRequest codeRequest) {
-        userService.verify(codeRequest);
+        userService.verify(codeRequest);        // 인증번호 검증
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/signup/final")
-    public ResponseEntity<Void> signUp(@RequestBody UserRequest userRequest) {
-        userService.update(userRequest);
+    public ResponseEntity<Void> signUp(@Valid @RequestBody UserRequest userRequest) {
+        userService.update(userRequest);        // 회원가입 완료
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/capstone/wanf/user/domain/entity/User.java
+++ src/main/java/com/capstone/wanf/user/domain/entity/User.java
@@ -3,6 +3,8 @@ package com.capstone.wanf.user.domain.entity;
 import com.capstone.wanf.common.entity.BaseTimeEntity;
 import com.capstone.wanf.user.dto.request.UserRequest;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,9 +19,11 @@ public class User extends BaseTimeEntity {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Email(message = "이메일 형식이 잘못되었습니다.")
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$", message = "비밀번호는 영문과 숫자를 섞어서 8자 이상 입력해야 합니다.")
     @Column(name = "user_password")
     private String userPassword;
 
