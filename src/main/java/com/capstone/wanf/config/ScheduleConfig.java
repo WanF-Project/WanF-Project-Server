b0f8c9a633df507fb6cb4715e3cd9803edf39534
kikingki <mongbu54@gmail.com>
2023-04-04 03:39:55 +0900

    fix: Scheduler가 정상 동작하지 않는 문제 해결

--- src/main/java/com/capstone/wanf/config/ScheduleConfig.java
+++ src/main/java/com/capstone/wanf/config/ScheduleConfig.java
@@ -3,6 +3,7 @@ package com.capstone.wanf.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 @Configuration
 @EnableScheduling
@@ -19,10 +22,14 @@ public class ScheduleConfig {
     private final JobLauncher jobLauncher;
     private final Job userDeleteJob;
 
+    // 매번 같은 JobParameters를 사용해서 Job이 중복 실행되지 않도록 막고 있어 정상적으로 동작하지 않음.
+    // JobParameters를 동적으로 생성하여 매번 다른 값을 사용하도록 변경 -> 현재 시간을 JobParameters로 넣어 매번 새로운 JobInstance가 생성됨
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 매일 자정에 실행
     public void deleteUnverifiedUserJob() throws JobParametersInvalidException, JobExecutionAlreadyRunningException,
             JobRestartException, JobInstanceAlreadyCompleteException {
-        jobLauncher.run(userDeleteJob, new JobParameters());
-        System.out.println("스케줄링");
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("time", LocalDateTime.now().toString())
+                .toJobParameters();
+        jobLauncher.run(userDeleteJob, jobParameters);
     }
 }
