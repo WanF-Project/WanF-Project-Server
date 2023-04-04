45688802f4ae40d205d08ca7d202f4e152cb3949
qwe916 <qwe916@naver.com>
2023-04-05 00:14:32 +0900

    refactor: Paging 삭제

--- src/main/java/com/capstone/wanf/course/service/CourseService.java
+++ src/main/java/com/capstone/wanf/course/service/CourseService.java
@@ -3,12 +3,12 @@ package com.capstone.wanf.course.service;
 import com.capstone.wanf.course.domain.entity.Course;
 import com.capstone.wanf.course.domain.repo.CourseRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +21,7 @@ public class CourseService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 수업이 존재하지 않습니다."));
     }
 
-    public Slice<Course> findAll(Pageable pageable) {
-        return courseRepository.findAll(pageable);
+    public List<Course> findAll() {
+        return courseRepository.findAll();
     }
 }
