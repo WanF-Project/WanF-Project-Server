2b14b8aa0581adbd6e693ddd6817034e0fff17c0
kikingki <mongbu54@gmail.com>
2023-04-09 00:43:13 +0900

    fix: 에러 코드 수정

--- src/main/java/com/capstone/wanf/error/errorcode/CommonErrorCode.java
+++ src/main/java/com/capstone/wanf/error/errorcode/CommonErrorCode.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {  // 애플리케이션 전역 사용
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    // 409 - 충돌
+    TRANSACTION_FAILED(HttpStatus.CONFLICT, "트랜잭션 커밋을 시도하는 동안 예외가 발생하여 변경에 실패했습니다."),
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 데이터를 찾을 수 없습니다."),
-
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러"),
-    
-    TRANSACTION_FAILED(HttpStatus.CONFLICT, "트랜잭션 커밋을 시도하는 동안 예외가 발생하여 변경에 실패했습니다.");
+    // 500 - 서버 에러
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러");
 
     private final HttpStatus httpStatus;
 
--- src/main/java/com/capstone/wanf/error/errorcode/CustomErrorCode.java
+++ src/main/java/com/capstone/wanf/error/errorcode/CustomErrorCode.java
@@ -7,8 +7,21 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CustomErrorCode implements ErrorCode {    // 특정 도메인 사용
+    // 400 - 잘못된 요청
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "인증번호를 다시 발급받아주세요."),
+
+    // 401 - 인증 실패
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 잘못 입력했습니다."),
+
+    // 404 - 요청받은 리소스를 찾을 수 없음
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 email로 가입된 유저가 존재하지 않습니다."),
+    VERIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "인증번호가 올바르지 않습니다."),
+    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 프로필이 존재하지 않습니다."),
+    MAJOR_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 전공이 존재하지 않습니다."),
+    COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 수업이 존재하지 않습니다."),
+
+    // 409 - 충돌
     DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "이미 가입된 이메일입니다.");
-//    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 잘못 입력했습니다.")
 
     private final HttpStatus httpStatus;
 
