e64e15d4a79a1507fa7171c719a5a33971e88e2a
qwe916 <qwe916@naver.com>
2023-04-11 22:37:47 +0900

    feat: post 엔티티에 category 컬럼 추가 및 BaseTimeEntity 상속

--- src/main/java/com/capstone/wanf/post/domain/entity/Post.java
+++ src/main/java/com/capstone/wanf/post/domain/entity/Post.java
@@ -1,5 +1,6 @@
 package com.capstone.wanf.post.domain.entity;
 
+import com.capstone.wanf.common.entity.BaseTimeEntity;
 import com.capstone.wanf.course.domain.entity.Course;
 import com.capstone.wanf.post.dto.request.RequestPost;
 import com.capstone.wanf.profile.domain.entity.Profile;
@@ -12,17 +13,20 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Post {
+public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "title", nullable = false)
+    @Column(name = "title")
     private String title;
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
     @OneToOne
     @JoinColumn(name = "course_id", referencedColumnName = "id")
