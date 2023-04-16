72b9026f835d8e634a407fef7df9f216048a0cc8
kikingki <mongbu54@gmail.com>
2023-04-17 01:40:06 +0900

    fix: json 객체로 반환하도록 수정

--- src/main/java/com/capstone/wanf/error/errorcode/CommonErrorCode.java
+++ src/main/java/com/capstone/wanf/error/errorcode/CommonErrorCode.java
@@ -8,6 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {  // 애플리케이션 전역 사용
+    // 401 - 인증 실패
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 토큰이 유효하지 않습니다."),
+
+    // 403 - 접근 권한 없음
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+
     // 409 - 충돌
     TRANSACTION_FAILED(HttpStatus.CONFLICT, "트랜잭션 커밋을 시도하는 동안 예외가 발생하여 변경에 실패했습니다."),
 
--- src/main/java/com/capstone/wanf/error/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/capstone/wanf/error/handler/JwtAccessDeniedHandler.java
@@ -1,13 +1,18 @@
 package com.capstone.wanf.error.handler;
 
+import com.capstone.wanf.error.errorcode.CommonErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Component
@@ -17,8 +22,21 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
 
         response.setCharacterEncoding("utf-8");
-        response.sendError(403, "접근 권한이 없습니다.");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode errorJson = mapper.createObjectNode();
+
+        errorJson.put("timestamp", LocalDateTime.now().toString());
+        errorJson.put("status", response.getStatus());
+        errorJson.put("code", CommonErrorCode.FORBIDDEN.name());
+        errorJson.put("message", CommonErrorCode.FORBIDDEN.getMessage());
+
+        response.getWriter().write(mapper.writeValueAsString(errorJson));
     }
 }
