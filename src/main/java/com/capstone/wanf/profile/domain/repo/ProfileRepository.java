f88014f41c7f70f3a985ac1a1aed39a17b12fb8b
qwe916 <qwe916@naver.com>
2023-04-18 01:27:01 +0900

    feat: User를 통해 Profile 찾는 메소드 추가

--- src/main/java/com/capstone/wanf/profile/domain/repo/ProfileRepository.java
+++ src/main/java/com/capstone/wanf/profile/domain/repo/ProfileRepository.java
@@ -1,7 +1,11 @@
 package com.capstone.wanf.profile.domain.repo;
 
 import com.capstone.wanf.profile.domain.entity.Profile;
+import com.capstone.wanf.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUser(User user);
 }
