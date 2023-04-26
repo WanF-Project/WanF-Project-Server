846526556e141ff78ab8a527f4fb0b5692773607
qwe916 <qwe916@naver.com>
2023-04-26 18:29:05 +0900

    docs: api 문서화

--- src/main/java/com/capstone/wanf/comment/controller/CommentController.java
+++ src/main/java/com/capstone/wanf/comment/controller/CommentController.java
@@ -5,6 +5,8 @@ import com.capstone.wanf.comment.dto.request.RequestComment;
 import com.capstone.wanf.comment.service.CommentService;
 import com.capstone.wanf.jwt.domain.UserDetailsImpl;
 import com.capstone.wanf.user.domain.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,6 +19,13 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("posts/{id}/comments")
+    @Operation(
+            summary = "댓글 작성",
+            description = "해당 게시글에 댓글을 작성합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공")
+            }
+    )
     public ResponseEntity<Comment> save(@PathVariable(name = "id") Long postId, @RequestBody RequestComment requestComment, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
 
@@ -26,6 +35,14 @@ public class CommentController {
     }
 
     @PatchMapping("/comments/{id}")
+    @Operation(
+            summary = "댓글 수정",
+            description = "해당 댓글을 수정합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공"),
+                    @ApiResponse(responseCode = "404", ref = "404")
+            }
+    )
     public ResponseEntity<Comment> update(@PathVariable(name = "id") Long id, @RequestBody RequestComment requestComment) {
         Comment comment = commentService.update(id, requestComment);
 
@@ -33,6 +50,13 @@ public class CommentController {
     }
 
     @DeleteMapping("/comments/{id}")
+    @Operation(
+            summary = "댓글 삭제",
+            description = "해당 댓글을 삭제합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공")
+            }
+    )
     public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
         commentService.delete(id);
 
--- src/main/java/com/capstone/wanf/profile/controller/ProfileController.java
+++ src/main/java/com/capstone/wanf/profile/controller/ProfileController.java
@@ -5,6 +5,8 @@ import com.capstone.wanf.profile.domain.entity.Profile;
 import com.capstone.wanf.profile.dto.request.ProfileRequest;
 import com.capstone.wanf.profile.service.ProfileService;
 import com.capstone.wanf.user.domain.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,14 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @GetMapping("/profiles/{id}")
+    @Operation(
+            summary = "특정 프로필 조회",
+            description = "해당 ID의 프로필을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공"),
+                    @ApiResponse(responseCode = "404", ref = "404")
+            }
+    )
     public ResponseEntity<Profile> findById(@PathVariable(name = "id") Long id) {
         Profile profile = profileService.findById(id);
 
@@ -25,6 +35,13 @@ public class ProfileController {
     }
 
     @PostMapping("/profiles")
+    @Operation(
+            summary = "프로필 생성",
+            description = "프로필을 생성합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공")
+            }
+    )
     public ResponseEntity<Profile> save(@Valid @RequestBody ProfileRequest profileRequest, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
 
@@ -34,6 +51,14 @@ public class ProfileController {
     }
 
     @PatchMapping("/profiles/{id}")
+    @Operation(
+            summary = "프로필 수정",
+            description = "프로필을 수정합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공"),
+                    @ApiResponse(responseCode = "404", ref = "404")
+            }
+    )
     public ResponseEntity<Profile> updateField(@PathVariable(name = "id") Long id, @Valid @RequestBody ProfileRequest profileRequest) {
         Profile profile = profileService.update(id, profileRequest);
 
