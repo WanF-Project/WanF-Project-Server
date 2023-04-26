3480050e940eff91217b780cac94fa05621cb8fc
kikingki <mongbu54@gmail.com>
2023-04-27 03:29:21 +0900

    feat: group 관련 entity 추가

---
+++ src/main/java/com/capstone/wanf/group/domain/entity/GroupAuth.java
@@ -0,0 +1,28 @@
+package com.capstone.wanf.group.domain.entity;
+
+import com.capstone.wanf.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "group")
+@Entity
+public class GroupAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "authority", nullable = false)
+    private String authority;
+}
