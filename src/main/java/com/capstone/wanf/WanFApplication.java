8c6e709949c28dda0e64049b07d40933ef15e548
qwe916 <qwe916@naver.com>
2023-04-30 18:47:30 +0900

    docs: swagger https 설정

--- src/main/java/com/capstone/wanf/WanFApplication.java
+++ src/main/java/com/capstone/wanf/WanFApplication.java
@@ -1,8 +1,11 @@
 package com.capstone.wanf;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = @Server(url = "{https://wanf-general-server.duckdns.org", description = "WanF General Server"))
 @SpringBootApplication
 public class WanFApplication {
 
