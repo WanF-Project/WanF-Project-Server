ccb40598f98e7399054912cbb8eb5d6ee66ca3e5
kikingki <mongbu54@gmail.com>
2023-05-08 04:46:25 +0900

    feat: Club의 request와 repository 추가

---
+++ src/main/java/com/capstone/wanf/club/domain/repo/ClubAuthRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.wanf.club.domain.repo;
+
+import com.capstone.wanf.club.domain.entity.ClubAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubAuthRepository extends JpaRepository<ClubAuth, Long> {
+}
