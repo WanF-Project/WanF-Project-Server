398db3f95a44d7c47e8bd54ba395909968f5a42c
kikingki <mongbu54@gmail.com>
2023-05-08 03:51:36 +0900

    refactor: 코드의 가독성 높임

--- src/main/java/com/capstone/wanf/club/domain/repo/ClubRepository.java
+++ src/main/java/com/capstone/wanf/club/domain/repo/ClubRepository.java
@@ -3,8 +3,5 @@ package com.capstone.wanf.club.domain.repo;
 import com.capstone.wanf.club.domain.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ClubRepository extends JpaRepository<Club, Long> {
-    List<Club> findAll();
 }
