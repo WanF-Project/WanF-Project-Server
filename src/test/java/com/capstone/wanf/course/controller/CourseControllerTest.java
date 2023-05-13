eb86c655503d52560bbf5a21c3a814a48b433018
hongyoung <qwe916@naver.com>
2023-05-14 04:01:44 +0900

    feat: Course, Major controller test 추가

--- src/test/java/com/capstone/wanf/course/controller/CourseControllerTest.java
+++ src/test/java/com/capstone/wanf/course/controller/CourseControllerTest.java
@@ -1,6 +1,7 @@
 package com.capstone.wanf.course.controller;
 
 import com.capstone.wanf.ControllerTest;
+import com.capstone.wanf.course.domain.entity.Course;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
@@ -26,7 +27,49 @@ class CourseControllerTest extends ControllerTest {
     void ID에_맞는_수업을_조회한다() {
         //given
         final String accessToken = getAccessToken();
-        // TODO: 2023/05/13 수업 생성 로직 추가
+
+        Course course = Course.builder()
+                .courseId("과목코드")
+                .name("수업명")
+                .courseTime("수업시간")
+                .professor("교수")
+                .category("카테고리")
+                .courseId("과목코드")
+                .build();
+
+        ExtractableResponse<Response> 수업_등록 = 수업_등록(accessToken, course);
+
+        //when
+        ExtractableResponse<Response> 수업_조회 = 수업_조회(accessToken, 수업_등록.jsonPath().getLong("id"));
+
+        //then
+        assertAll(
+                () -> assertThat(수업_조회.statusCode()).isEqualTo(200),
+                () -> assertThat(수업_조회.jsonPath().getLong("id")).isEqualTo(수업_등록.jsonPath().getLong("id"))
+        );
+    }
+
+    @Test
+    void 수업을_삭제한다() {
+        //given
+        final String accessToken = getAccessToken();
+
+        Course course = Course.builder()
+                .courseId("과목코드")
+                .name("수업명")
+                .courseTime("수업시간")
+                .professor("교수")
+                .category("카테고리")
+                .courseId("과목코드")
+                .build();
+
+        ExtractableResponse<Response> 수업_등록 = 수업_등록(accessToken, course);
+
+        //when
+        ExtractableResponse<Response> 수업_삭제 = 수업_삭제(accessToken, 수업_등록.jsonPath().getLong("id"));
+
+        //then
+        assertThat(수업_삭제.statusCode()).isEqualTo(204);
     }
 
     @Test
