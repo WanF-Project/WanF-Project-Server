8e58b8a2862f12b8934b71451a2eb4ae760ab563
hongyoung <qwe916@naver.com>
2023-05-27 02:54:15 +0900

    feat: Course 추가 및 삭제 로직 테스트

--- src/test/java/com/capstone/wanf/fixture/DomainFixture.java
+++ src/test/java/com/capstone/wanf/fixture/DomainFixture.java
@@ -2,6 +2,7 @@ package com.capstone.wanf.fixture;
 
 import com.capstone.wanf.course.domain.entity.Course;
 import com.capstone.wanf.course.domain.entity.Major;
+import com.capstone.wanf.course.dto.request.RequestCourse;
 
 public class DomainFixture {
     public static final Major 전공1 = Major.builder()
@@ -15,4 +16,8 @@ public class DomainFixture {
             .courseId("과목코드1")
             .professor("교수1")
             .build();
+
+    public static final RequestCourse 수업_요청1 = new RequestCourse("수업명", "카테고리", "수업시간", "과목코드", "교수");
+
+
 }
