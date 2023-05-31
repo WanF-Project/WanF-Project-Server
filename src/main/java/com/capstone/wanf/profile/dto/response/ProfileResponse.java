35365eae57329669b4a445a0c8e906bd4da81e62
qwe916 <qwe916@naver.com>
2023-05-31 23:53:50 +0900

    fix: ProfileResponse record로 변경

---
+++ src/main/java/com/capstone/wanf/profile/dto/response/ProfileResponse.java
@@ -0,0 +1,36 @@
+package com.capstone.wanf.profile.dto.response;
+
+import com.capstone.wanf.course.domain.entity.Major;
+import com.capstone.wanf.profile.domain.entity.MBTI;
+import com.capstone.wanf.profile.domain.entity.ProfileImage;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.util.List;
+@Builder
+@Schema(description = "프로필 응답")
+public record ProfileResponse(
+        @Schema(description = "프로필 ID", example = "1")
+        Long id,
+        @Schema(description = "닉네임", example = "김코딩")
+        String nickname,
+        @Schema(description = "학번", example = "2019101234")
+        Integer studentId,
+        @Schema(description = "나이", example = "23")
+        Integer age,
+        @Schema(description = "연락처", example = "010-1234-5678")
+        String contact,
+        @Schema(description = "프로필 이미지", example = "BEAR")
+        ProfileImage profileImage,
+        @Schema(description = "성별", example = "남성")
+        String gender,
+        @Schema(description = "MBTI", example = "ISTJ")
+        MBTI mbti,
+        @Schema(description = "성격", example = "외향적인")
+        List<String> personalities,
+        @Schema(description = "목표", example = "취업")
+        List<String> goals,
+        @Schema(description = "전공", example = "컴퓨터공학과")
+        Major major
+) {
+}
