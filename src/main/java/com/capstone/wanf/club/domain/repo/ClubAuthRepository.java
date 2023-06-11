321281375750d1ccbbc9f9e8ef8ef395ead0ac5e
kikingki <mongbu54@gmail.com>
2023-06-12 02:43:04 +0900

    feat: 모임 권한에 대한 로직 추가

--- src/main/java/com/capstone/wanf/club/domain/repo/ClubAuthRepository.java
+++ src/main/java/com/capstone/wanf/club/domain/repo/ClubAuthRepository.java
@@ -3,5 +3,11 @@ package com.capstone.wanf.club.domain.repo;
 import com.capstone.wanf.club.domain.entity.ClubAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ClubAuthRepository extends JpaRepository<ClubAuth, Long> {
+    List<ClubAuth> findByUserId(Long userId);
+
+    Optional<ClubAuth> findByUserIdAndClubId(Long userId, Long clubId);
 }
--- src/main/java/com/capstone/wanf/club/service/ClubAuthService.java
+++ src/main/java/com/capstone/wanf/club/service/ClubAuthService.java
@@ -3,10 +3,15 @@ package com.capstone.wanf.club.service;
 import com.capstone.wanf.club.domain.entity.Club;
 import com.capstone.wanf.club.domain.entity.ClubAuth;
 import com.capstone.wanf.club.domain.repo.ClubAuthRepository;
+import com.capstone.wanf.error.exception.RestApiException;
 import com.capstone.wanf.user.domain.entity.User;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.capstone.wanf.error.errorcode.CommonErrorCode.FORBIDDEN;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +27,30 @@ public class ClubAuthService {
                 .build();
 
         clubAuthRepository.save(clubAuth);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClubAuth> findByUserId(Long userId) {
+        List<ClubAuth> clubAuthList = clubAuthRepository.findByUserId(userId);
+
+        return clubAuthList;
+    }
+
+    @Transactional(readOnly = true)
+    public String findByUserIdAndClubId(Long userId, Long clubId) {
+        ClubAuth userAuth = clubAuthRepository.findByUserIdAndClubId(userId, clubId).orElse(null);
+
+        if (userAuth == null) return "권한 없음";
+
+        return userAuth.getAuthority();
+    }
+
+    @Transactional(readOnly = true)
+    public String getAuthority(Long userId, Long clubId) {
+        String userAuth = findByUserIdAndClubId(userId, clubId);
+
+        if (userAuth.equals("권한 없음")) throw new RestApiException(FORBIDDEN);
+
+        else return userAuth;
     }
 }
