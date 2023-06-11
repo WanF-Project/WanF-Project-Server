1ddf21996d9d9571e2837e838e9a73b1b059d37f
qwe916 <qwe916@naver.com>
2023-06-12 05:56:44 +0900

    fix: Comment 삭제와 수정 로직 변경

--- src/main/java/com/capstone/wanf/comment/service/CommentService.java
+++ src/main/java/com/capstone/wanf/comment/service/CommentService.java
@@ -1,7 +1,6 @@
 package com.capstone.wanf.comment.service;
 
 import com.capstone.wanf.comment.domain.entity.Comment;
-import com.capstone.wanf.comment.domain.repo.CommentRepository;
 import com.capstone.wanf.comment.dto.request.CommentRequest;
 import com.capstone.wanf.error.exception.RestApiException;
 import com.capstone.wanf.post.domain.entity.Post;
@@ -18,8 +17,6 @@ import static com.capstone.wanf.error.errorcode.CustomErrorCode.COMMENT_NOT_FOUN
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-    private final CommentRepository commentRepository;
-
     private final PostService postService;
 
     private final ProfileService profileService;
@@ -32,32 +29,31 @@ public class CommentService {
 
         Comment comment = Comment.builder()
                 .content(commentRequest.content())
-                .post(post)
                 .profile(profile)
                 .build();
 
-        Comment saveComment = commentRepository.save(comment);
+        post.getComments().add(comment);
 
-        post.getComments().add(saveComment);
-
-        return saveComment;
+        return comment;
     }
 
-    public Comment update(Long id, CommentRequest commentRequest) {
-        Comment comment = commentRepository.findById(id)
+    @Transactional
+    public Comment update(Long postId, Long commentId, CommentRequest commentRequest) {
+        Post post = postService.findById(postId);
+
+        Comment findComment = post.getComments().stream()
+                .filter(comment -> comment.getId() == commentId)
+                .findFirst()
                 .orElseThrow(() -> new RestApiException(COMMENT_NOT_FOUND));
 
-        comment.update(commentRequest.content());
+        findComment.update(commentRequest.content());
 
-        Comment updateComment = commentRepository.save(comment);
-
-        return updateComment;
+        return findComment;
     }
 
-    public void delete(Long id) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new RestApiException(COMMENT_NOT_FOUND));
-
-        commentRepository.delete(comment);
+    @Transactional
+    public void delete(Long postId, Long commentId) {
+        postService.findById(postId).getComments()
+                .removeIf(comment -> comment.getId() == commentId);
     }
 }
