aa52a58e39e0ea4fc2cce7200610f5719fdfd027
hongyoung <qwe916@naver.com>
2023-08-03 22:46:43 +0900

    feat: Message Repository 추가

---
+++ src/main/java/com/capstone/wanf/message/domain/repo/MessageRepositorySupport.java
@@ -0,0 +1,27 @@
+package com.capstone.wanf.message.domain.repo;
+
+import com.capstone.wanf.message.domain.entity.QMessage;
+import com.capstone.wanf.user.domain.entity.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MessageRepositorySupport {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QMessage message = QMessage.message;
+
+    public List<User> findSenderByReceiver(User user) {
+        List<User> senders = jpaQueryFactory.select(message.sender)
+                .from(message)
+                .where(message.receiver.eq(user))
+                .distinct()
+                .fetch();
+
+        return senders;
+    }
+}
