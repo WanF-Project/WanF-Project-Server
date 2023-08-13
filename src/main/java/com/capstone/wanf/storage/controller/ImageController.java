bf7125f804f0797f89c63e9bf2eb2b79d7b6711a
kikingki <mongbu54@gmail.com>
2023-08-13 12:07:28 +0900

    docs: 이미지 업로드 swagger annotation 추가

--- src/main/java/com/capstone/wanf/storage/controller/ImageController.java
+++ src/main/java/com/capstone/wanf/storage/controller/ImageController.java
@@ -3,6 +3,8 @@ package com.capstone.wanf.storage.controller;
 import com.capstone.wanf.storage.domain.Directory;
 import com.capstone.wanf.storage.dto.response.ImageResponse;
 import com.capstone.wanf.storage.service.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,15 @@ public class ImageController {
     private final S3Service s3Service;
 
     @PostMapping(value = "/images", consumes = {"multipart/form-data"})
+    @Operation(
+            summary = "이미지 업로드",
+            description = "이미지를 업로드합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공"),
+                    @ApiResponse(responseCode = "400", ref = "400"),
+                    @ApiResponse(responseCode = "403", ref = "403")
+            }
+    )
     public ResponseEntity<ImageResponse> imageUpload(@RequestParam("directory") Directory directory,
                                                      @RequestPart("uploadImage") MultipartFile uploadImage) {   // RequestPart: MultipartFile이 포함되는 경우
         String imageUrl = s3Service.upload(uploadImage, directory);
